9f7b2374c797ef3d503858f2c291f166627ba0f6
Ozel Yilmazel <ozelyilmazel@vl965-172-31-234-17.wireless.umass.edu>
2025-02-21 12:53:32 -0500

    plots

--- normalize_data.py
+++ normalize_data.py
@@ -5,6 +5,7 @@ from nltk.stem import WordNetLemmatizer
 from pathlib import Path
 import re
 
+
 def download_nltk_resources():
     try:
         nltk.download("stopwords", quiet=True)
@@ -12,7 +13,9 @@ def download_nltk_resources():
     except Exception as e:
         print(f"Error downloading NLTK resources: {e}")
 
+
 download_nltk_resources()
+
 
 def normalize_transcript(transcript: str) -> str:
     transcript = re.sub(r"[^\w\s]|(?<!\d)\d(?!\d)", "", transcript)
@@ -25,17 +28,18 @@ def normalize_transcript(transcript: str) -> str:
     transcript = re.sub(r"\s+", " ", transcript).strip()
     return transcript
 
+
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
-transcript_dir = Path("/work/pi_vcpartridge_umass_edu/ytb_csv/")
+transcript_dir = Path("data/")
 try:
     df = pd.read_csv(transcript_dir / "transcripts.csv", header=0)
-    df.dropna(subset=["transcript"], inplace=True)
 except FileNotFoundError:
     print("Error: transcripts.csv file not found.")
     df = pd.DataFrame()
 
 df["transcript"] = df["transcript"].apply(normalize_transcript)
+df.dropna(subset=["transcript"], inplace=True)
 
 df.to_csv(transcript_dir / "transcripts_normalized.csv", index=False)
