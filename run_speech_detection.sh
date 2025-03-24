74765dd8a90baf80015ebcfaa1216a52a2fc916d
oyilmazel_umass_edu <ozel@yilmazel.com>
2025-03-24 15:33:39 +0000

    speech detection results

--- run_speech_detection.sh
+++ run_speech_detection.sh
@@ -3,7 +3,7 @@
 #SBATCH -c 16
 #SBATCH --mem=32GB
 #SBATCH -p cpu-preempt
-#SBATCH --time 05:00:00
+#SBATCH --time 8:00:00
 #SBATCH -o run_%j.out
 
 module load conda/latest
@@ -11,4 +11,4 @@ module load uri/main all/FFmpeg/4.2.1-GCCcore-8.3.0
 
 conda activate ytb
 
-python kmeans_speech.py
+python speech_detection.py
--- speech_detection.py
+++ speech_detection.py
@@ -2,32 +2,36 @@ from pyannote.audio.pipelines import VoiceActivityDetection
 from pyannote.audio import Model
 from pyannote.core.annotation import Annotation
 from dotenv import load_dotenv
+import pandas as pd
+from tqdm import tqdm
 import os
 
 load_dotenv()
 
-model = Model.from_pretrained("pyannote/segmentation-3.0", use_auth_token=os.getenv("PYANNOTE_TOKEN"))
+WAV_PATH = "../../../work/pi_vcpartridge_umass_edu/ytb_wavs/"
+
+wav_files = [f for f in os.listdir(WAV_PATH) if f.endswith(".wav")]
+
+model = Model.from_pretrained(
+    "pyannote/segmentation-3.0", use_auth_token=os.getenv("PYANNOTE_TOKEN")
+)
 pipeline = VoiceActivityDetection(segmentation=model)
 
-examples = [
-    "apQoQ-MR1fA.wav"
-]
-
 HYPER_PARAMETERS = {
-  # remove speech regions shorter than that many seconds.
-  "min_duration_on": 1.0,
-  # fill non-speech regions shorter than that many seconds.
-  "min_duration_off": 0.0
+    # remove speech regions shorter than that many seconds.
+    "min_duration_on": 1.0,
+    # fill non-speech regions shorter than that many seconds.
+    "min_duration_off": 0.0,
 }
 
 pipeline.instantiate(HYPER_PARAMETERS)
 
 results = []
 
-for example in examples:
-    vad = pipeline(example)
-    results.append(vad)
+for wav_file in tqdm(wav_files):
+    vad = pipeline(WAV_PATH + wav_file)
+    total_speech_duration = vad.get_timeline().duration()
+    results.append((wav_file.replace(".wav", ""), total_speech_duration))
 
-print(results[0])
-print(len(results[0]))
-print(results[0].get_timeline().duration())
+df = pd.DataFrame(results, columns=["video_id", "speech_duration"])
+df.to_csv("speech_detection_results.csv", index=False)
